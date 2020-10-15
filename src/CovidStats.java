424a2043f102fe64f09ad8a5a06bcdc9be995537
sarawarnock <sara@sarawarnock.com>
2020-10-15 13:58:13 -0600

    imports

--- src/CovidStats.java
+++ src/CovidStats.java
@@ -1,5 +1,23 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class CovidStats {
     public static void main(String[] args) {
+        int [] covid19 = null;
+        try {
+            //read the data from the file
+            Path path = Paths.get("data.txt");
+            Stream<String> lines = Files.lines(path);
 
+            //convert to array of int
+            covid19 = lines.mapToInt(Integer::path);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(covid19.length);
+        //all code to work with array goes here
     }
 }
