dc33c6323bfcad5825ebc3c89c4be83fbbc4ae6a
sarawarnock <sara@sarawarnock.com>
2020-10-15 11:52:01 -0600

    first commit

---
+++ src/CovidStats.java
@@ -0,0 +1,5 @@
+public class CovidStats {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/TryItOut.java
@@ -0,0 +1,15 @@
+public class TryItOut {
+    public static void main(String[] args) {
+        int[] numbers;
+        numbers = new int[8];
+        numbers[0] = 19;
+        numbers[1] = 12;
+        numbers[2] = 72;
+        numbers[3] = 68;
+        numbers[4] = 44;
+        numbers[5] = 92;
+        numbers[6] = 54;
+        numbers[7] = 36;
+        System.out.println(numbers.length);
+    }
+}
